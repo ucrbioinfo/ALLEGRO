b3cd564fa1de8af2a25ba1ba7eeb190479e69a8b
AmirUCR <amohs002@ucr.edu>
2023-01-16 16:31:10 -0500

    Name change, formatting

--- src/scorers/scorer_factory.py
+++ src/scorers/scorer_factory.py
@@ -10,16 +10,14 @@ class ScorerFactory:
         pass
 
 
-    def get_scorer(self, scorer_name: str, scorer_settings: dict[str, str]) -> Scorer:
+    def make_scorer(self, scorer_name: str, scorer_settings: dict[str, str]) -> Scorer:
         print('Selected scorer:', scorer_name)
 
         match scorer_name:
             case 'chopchop':
                 return ChopChopWrapper(scorer_settings)
-            # case 'deepguide_1':
-            #     return DeepGuideWrapper(scorer_settings)
             case 'random':
                 return RandomScorer()  # Assigns all guides a random score between [0, 10)
             case _:
-                print('Unknown scorer selected. Assigning all guides a default score of 1.0')
+                print('Unknown/dummy scorer selected. Assigning all guides a default score of 1.0')
                 return DummyScorer()
