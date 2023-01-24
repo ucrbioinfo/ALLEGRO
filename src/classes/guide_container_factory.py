f9f3cded07b8fe36dab4cb01d1dfa84de62882ba
AmirUCR <amohs002@ucr.edu>
2023-01-24 18:53:05 -0500

    add comments, add ortho to prot regex

--- src/classes/guide_container_factory.py
+++ src/classes/guide_container_factory.py
@@ -32,21 +32,31 @@ class GuideContainerFactory:
                 tag_regex = r'\[locus_tag=(.*?)\]'
                 protein_id_regex = r'\[protein_id=(.*?)\]'
                 reference_species_regex = r'\[ref_species=(.*?)\]'
-                orthologous_name_regex = r'\[orthologous_to=(.*?)\]'
+                orthologous_name_regex = r'\[orthologous_to_gene=(.*?)\]'
+                orthologous_protein_regex = r'\[orthologous_to_ref_protein=(.*?)\]'
 
                 for id, cds_record in enumerate(records):
+                    # Locus tag example: [locus_tag=KLMA_50610], extracts KLMA_50610
                     tag_match = re.search(tag_regex, cds_record.description)
                     locus_tag = tag_match.group(1) if tag_match is not None else 'N/A'
 
+                    # This gene's own name -- Usually N/A for unannotated CDS files
                     gene_match = re.search(gene_regex, cds_record.description)
                     gene_name = gene_match.group(1) if gene_match is not None else 'N/A'
 
-                    ortho_to_match = re.search(orthologous_name_regex, cds_record.description)
-                    ortho_name = ortho_to_match.group(1) if ortho_to_match is not None else 'N/A'
-
+                    # This gene's own protein id
                     protein_id_match = re.search(protein_id_regex, cds_record.description)
                     protein_id = protein_id_match.group(1) if protein_id_match is not None else 'N/A'
 
+                    # For example, [orthologous_to_ref_protein=XP_022674739.1], extracts XP_022674739.1
+                    ortho_prot_to_match = re.search(orthologous_protein_regex, cds_record.description)
+                    ortho_prot_id = ortho_prot_to_match.group(1) if ortho_prot_to_match is not None else 'N/A'
+
+                    # For example, [orthologous_to_gene=HIS7], extracts HIS7
+                    ortho_gene_to_match = re.search(orthologous_name_regex, cds_record.description)
+                    ortho_gene_name = ortho_gene_to_match.group(1) if ortho_gene_to_match is not None else 'N/A'
+
+                    # For example, [ref_species=kluyveromyces_marxianus], extracts kluyveromyces_marxianus
                     reference_species_match = re.search(reference_species_regex, cds_record.description)
                     ref_species = reference_species_match.group(1) if reference_species_match is not None else 'N/A'
 
@@ -58,9 +68,10 @@ class GuideContainerFactory:
                         species=species_object,
                         string_id=cds_record.id,
                         ref_species=ref_species,
-                        orthologous_to=ortho_name,
                         sequence=str(cds_record.seq),
                         guide_scorer=guide_scorer_obj,
+                        orthologous_to_prot=ortho_prot_id,
+                        orthologous_to_gene=ortho_gene_name,
                         )
                     )
 
