53f3cc7353b1c0cffa1f345dfe24efb9167e3547
AmirUCR <amohs002@ucr.edu>
2023-01-24 18:52:40 -0500

    add ortho to prot field, attribute name change

--- src/classes/gene.py
+++ src/classes/gene.py
@@ -20,8 +20,9 @@ class Gene(GuideContainer):
         protein_id: str,
         species: Species,
         ref_species: str,
-        orthologous_to: str,
         guide_scorer: Scorer,
+        orthologous_to_gene: str,
+        orthologous_to_prot: str,
         ) -> None:
 
         self.sequence = sequence
@@ -32,8 +33,9 @@ class Gene(GuideContainer):
         self.protein_id = protein_id
         self.species = species
         self.ref_species = ref_species
-        self.orthologous_to = orthologous_to
-        self.guide_scorer = guide_scorer    
+        self.guide_scorer = guide_scorer
+        self.orthologous_to_gene = orthologous_to_gene
+        self.orthologous_to_prot = orthologous_to_prot
 
         self.cas9_guide_objects: list[Guide] = list()
 
@@ -50,11 +52,11 @@ class Gene(GuideContainer):
             for guide_strand_score_tuple in guide_strand_score_tuple_list:
                 self.cas9_guide_objects.append(Guide(
                     pam='GG',
+                    container=self,
                     endonuclease='cas9',
                     score=guide_strand_score_tuple[2],
                     strand=guide_strand_score_tuple[1],
                     sequence=guide_strand_score_tuple[0],
-                    container=self,
                     )
                 )
             
@@ -63,12 +65,13 @@ class Gene(GuideContainer):
 
     def get_attributes_dict(self) -> dict:
         return dict({
-            'gene_name': self.gene_name,
+            'cds_name': self.gene_name,
             'protein_id': self.protein_id,
-            'gene_string_id': self.string_id,
-            'gene_locus_tag': self.locus_tag,
-            'gene_integer_id': self.integer_id,
-            'gene_ref_species': self.ref_species,
-            'gene_species_name': self.species_name,
-            'gene_orthologous_to': self.orthologous_to,
+            'cds_string_id': self.string_id,
+            'cds_locus_tag': self.locus_tag,
+            'cds_integer_id': self.integer_id,
+            'cds_ref_species': self.ref_species,
+            'cds_species_name': self.species_name,
+            'cds_orthologous_to': self.orthologous_to_gene,
+            'cds_orthologous_to_ref_prot_id': self.orthologous_to_prot,
         })
