be0439b9e4b7bf612451a6a32947328f99ca4623
AmirUCR <amohs002@ucr.edu>
2023-01-23 20:42:34 -0500

    remove redundant getters

--- src/classes/species.py
+++ src/classes/species.py
@@ -1,26 +1,26 @@
 from classes.guide import Guide
+from scorers.scorer_base import Scorer
 from classes.guide_container import GuideContainer
 from classes.guide_container_factory import GuideContainerFactory
-from scorers.scorer_base import Scorer
 
 
 class Species:
     def __init__(
         self,
+        id: int, 
+        name: str,
+        cds_path: str,
+        genome_path: str,
         guide_scorer: Scorer,
         guide_container_factory: GuideContainerFactory,
-        name: str, 
-        id: int, 
-        genome_path: str,
-        cds_path: str,
         ) -> None:
         
+        self.id = id
+        self.name = name
+        self.cds_path = cds_path
+        self.genome_path = genome_path
         self.guide_scorer = guide_scorer
         self.guide_container_factory = guide_container_factory
-        self.name = name
-        self.id = id
-        self.genome_path = genome_path
-        self.cds_path = cds_path
 
         self.guide_containers: list[GuideContainer] = list()
 
@@ -33,27 +33,3 @@ class Species:
             cas9_guides_list.extend(container.get_cas9_guides())
 
         return cas9_guides_list
-    
-    
-    def get_scorer(self) -> Scorer:
-        return self.guide_scorer
-
-    
-    def get_genome_path(self) -> str:
-        return self.genome_path
-
-    
-    def get_cds_path(self) -> str:
-        return self.cds_path
-
-
-    def get_name(self) -> str:
-        return self.name
-
-
-    def get_id(self) -> int:
-        return self.id
-
-    
-    def get_guide_containers(self) -> list[GuideContainer]:
-        return self.guide_containers
