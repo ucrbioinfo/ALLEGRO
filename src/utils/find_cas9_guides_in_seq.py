2e77b1eb6153c2e34ac3bcc44bffceeda701dbb9
AmirUCR <amohs002@ucr.edu>
2023-01-24 20:48:25 -0500

    place functions in a class, add support for guide context

--- src/utils/find_cas9_guides_in_seq.py
+++ src/utils/find_cas9_guides_in_seq.py
@@ -5,125 +5,166 @@ import numpy
 from Bio.Seq import Seq
 
 
-def find_guides_on_both_strands(sequence: str, pam_regex: str = r'(?=(GG))', toward_5_prime: int = 21) -> list[str]:
-    guides_list: list[str] = list()
 
-    # Store the reverse complement
-    sequence_rev_comp = str(Seq(sequence).reverse_complement())
-
-    # Find PAMs on the forward strand
-    matches = re.finditer(pam_regex, sequence)
-    pam_positions = [match.start() for match in matches]
-
-    for position in pam_positions:
-        if (position - toward_5_prime >= 0):
-            # -21 and -1 because it's NGG and we find GG,
-            # we need to discard N and take the next 20 letters.
-            guide = sequence[position-toward_5_prime:position-1]  
-            guides_list.append(guide)
-
-    # Find PAMs on the reverse comp. strand
-    matches = re.finditer(pam_regex, sequence_rev_comp)
-    pam_positions = [match.start() for match in matches]
-
-    for position in pam_positions:
-        if (position - toward_5_prime >= 0):
-            guide = sequence_rev_comp[position-toward_5_prime:position-1]
-            guides_list.append(guide)
-
-    return guides_list
+class GuideFinder:
+    def __init__(self) -> None:
+        guide_to_guide_with_context_dict: dict[str, str] = dict()
 
 
-def find_guides_on_forward_strand(sequence: str, pam_regex: str = r'(?=(GG))', toward_5_prime: int = 21) -> list[str]:
-    guides_list: list[str] = list()
+    def find_guides_on_both_strands(
+        self,
+        sequence: str,
+        pam_regex: str = r'(?=(GG))',
+        toward_5_prime_from_pam: int = 20,
+        context_toward_5_prime_from_protospacer: int = 0,
+        context_toward_3_prime_from_pam: int = 0,
+        ) -> list[str]:
 
-    # Find PAMs on the forward strand
-    matches = re.finditer(pam_regex, sequence)
-    pam_positions = [match.start() for match in matches]
+        guides_list: list[str] = list()
 
-    for position in pam_positions:
-        if (position - toward_5_prime >= 0):
-            # -21 and -1 because it's NGG and we find GG,
-            # we need to discard N and take the next 20 letters.
-            guide = sequence[position-toward_5_prime:position-1]  
-            guides_list.append(guide)
+        # Store the reverse complement
+        sequence_rev_comp = str(Seq(sequence).reverse_complement())
 
-    return guides_list
+        # Find PAMs on the forward strand
+        matches = re.finditer(pam_regex, sequence)
+        pam_positions = [match.start() for match in matches]
 
+        for position in pam_positions:
+            if (position - toward_5_prime_from_pam >= 0):
+                # -20 -1 toward 5' and position-1 because it's NGG and we find GG,
+                # we need to discard N and take the next 20 letters.
+                guide = sequence[position-toward_5_prime_from_pam-1:position-1]
+                guide_with_context = sequence[position-toward_5_prime_from_pam-1-context_toward_5_prime_from_protospacer:position-1+context_toward_3_prime_from_pam]
+                
+                guides_list.append(guide)
+                self.guide_to_guide_with_context_dict[guide] = guide_with_context
+                
 
-def find_guides_on_reverse_strand(sequence: str, pam_regex: str = r'(?=(GG))', toward_5_prime: int = 21) -> list[str]:
-    guides_list: list[str] = list()
+        # Find PAMs on the reverse comp. strand
+        matches = re.finditer(pam_regex, sequence_rev_comp)
+        pam_positions = [match.start() for match in matches]
 
-    # Store the reverse complement
-    sequence_rev_comp = str(Seq(sequence).reverse_complement())
+        for position in pam_positions:
+            if (position - toward_5_prime_from_pam >= 0):
+                guide = sequence_rev_comp[position-toward_5_prime_from_pam:position-1]
+                guides_list.append(guide)
 
-    # Find PAMs on the reverse comp. strand
-    matches = re.finditer(pam_regex, sequence_rev_comp)
-    pam_positions = [match.start() for match in matches]
-
-    for position in pam_positions:
-        if (position - toward_5_prime >= 0):
-            guide = sequence_rev_comp[position-toward_5_prime:position-1]
-            guides_list.append(guide)
-
-    return guides_list
-
-
-def find_guides_and_indicate_strand(sequence: str, pam_regex: str = r'(?=(GG))', toward_5_prime: int = 21) -> list[tuple[str, str, float]]:
-    guides_list: list[tuple[str, str, float]] = list()
-
-    # Store the reverse complement
-    sequence_rev_comp = str(Seq(sequence).reverse_complement())
-
-    # Find PAMs on the forward strand
-    matches = re.finditer(pam_regex, sequence)
-    pam_positions = [match.start() for match in matches]
-
-    for position in pam_positions:
-        if (position - toward_5_prime >= 0):
-            # -21 and -1 because it's NGG and we find GG,
-            # we need to discard N and take the next 20 letters.
-            guide = sequence[position-toward_5_prime:position-1]  
-            guides_list.append((guide, 'F', 1.0))
-
-    # Find PAMs on the reverse comp. strand
-    matches = re.finditer(pam_regex, sequence_rev_comp)
-    pam_positions = [match.start() for match in matches]
-
-    for position in pam_positions:
-        if (position - toward_5_prime >= 0):
-            guide = sequence_rev_comp[position-toward_5_prime:position-1]
-            guides_list.append((guide, 'R', 1.0))
-
-    return guides_list
+        return guides_list
 
 
+    def find_guides_on_forward_strand(
+        self,
+        sequence: str,
+        pam_regex: str = r'(?=(GG))',
+        toward_5_prime: int = 21,
+        ) -> list[str]:
 
-def find_guides_and_indicate_strand_random_scores(sequence: str, pam_regex: str = r'(?=(GG))', toward_5_prime: int = 21) -> list[tuple[str, str, float]]:
-    guides_list: list[tuple[str, str, float]] = list()
+        guides_list: list[str] = list()
 
-    # Store the reverse complement
-    sequence_rev_comp = str(Seq(sequence).reverse_complement())
+        # Find PAMs on the forward strand
+        matches = re.finditer(pam_regex, sequence)
+        pam_positions = [match.start() for match in matches]
 
-    # Find PAMs on the forward strand
-    matches = re.finditer(pam_regex, sequence)
-    pam_positions = [match.start() for match in matches]
+        for position in pam_positions:
+            if (position - toward_5_prime >= 0):
+                # -21 and -1 because it's NGG and we find GG,
+                # we need to discard N and take the next 20 letters.
+                guide = sequence[position-toward_5_prime:position-1]  
+                guides_list.append(guide)
 
-    for position in pam_positions:
-        if (position - toward_5_prime >= 0):
-            # -21 and -1 because it's NGG and we find GG,
-            # we need to discard N and take the next 20 letters.
-            guide = sequence[position-toward_5_prime:position-1]
+        return guides_list
 
-            guides_list.append((guide, 'F', numpy.random.rand(1)[0] * 10))
 
-    # Find PAMs on the reverse comp. strand
-    matches = re.finditer(pam_regex, sequence_rev_comp)
-    pam_positions = [match.start() for match in matches]
+    def find_guides_on_reverse_strand(
+        self,
+        sequence: str,
+        pam_regex: str = r'(?=(GG))', 
+        toward_5_prime: int = 21,
+        ) -> list[str]:
 
-    for position in pam_positions:
-        if (position - toward_5_prime >= 0):
-            guide = sequence_rev_comp[position-toward_5_prime:position-1]
-            guides_list.append((guide, 'R', numpy.random.rand(1)[0] * 10))
+        guides_list: list[str] = list()
 
-    return guides_list
+        # Store the reverse complement
+        sequence_rev_comp = str(Seq(sequence).reverse_complement())
+
+        # Find PAMs on the reverse comp. strand
+        matches = re.finditer(pam_regex, sequence_rev_comp)
+        pam_positions = [match.start() for match in matches]
+
+        for position in pam_positions:
+            if (position - toward_5_prime >= 0):
+                guide = sequence_rev_comp[position-toward_5_prime:position-1]
+                guides_list.append(guide)
+
+        return guides_list
+
+
+    def find_guides_and_indicate_strand(
+        self,
+        sequence: str,
+        pam_regex: str = r'(?=(GG))',
+        toward_5_prime: int = 21,
+        ) -> list[tuple[str, str, float]]:
+
+        guides_list: list[tuple[str, str, float]] = list()
+
+        # Store the reverse complement
+        sequence_rev_comp = str(Seq(sequence).reverse_complement())
+
+        # Find PAMs on the forward strand
+        matches = re.finditer(pam_regex, sequence)
+        pam_positions = [match.start() for match in matches]
+
+        for position in pam_positions:
+            if (position - toward_5_prime >= 0):
+                # -21 and -1 because it's NGG and we find GG,
+                # we need to discard N and take the next 20 letters.
+                guide = sequence[position-toward_5_prime:position-1]  
+                guides_list.append((guide, 'F', 1.0))
+
+        # Find PAMs on the reverse comp. strand
+        matches = re.finditer(pam_regex, sequence_rev_comp)
+        pam_positions = [match.start() for match in matches]
+
+        for position in pam_positions:
+            if (position - toward_5_prime >= 0):
+                guide = sequence_rev_comp[position-toward_5_prime:position-1]
+                guides_list.append((guide, 'R', 1.0))
+
+        return guides_list
+
+
+    def find_guides_and_indicate_strand_random_scores(
+        self,
+        sequence: str,
+        pam_regex: str = r'(?=(GG))',
+        toward_5_prime: int = 21,
+        ) -> list[tuple[str, str, float]]:
+
+        guides_list: list[tuple[str, str, float]] = list()
+
+        # Store the reverse complement
+        sequence_rev_comp = str(Seq(sequence).reverse_complement())
+
+        # Find PAMs on the forward strand
+        matches = re.finditer(pam_regex, sequence)
+        pam_positions = [match.start() for match in matches]
+
+        for position in pam_positions:
+            if (position - toward_5_prime >= 0):
+                # -21 and -1 because it's NGG and we find GG,
+                # we need to discard N and take the next 20 letters.
+                guide = sequence[position-toward_5_prime:position-1]
+
+                guides_list.append((guide, 'F', numpy.random.rand(1)[0] * 10))
+
+        # Find PAMs on the reverse comp. strand
+        matches = re.finditer(pam_regex, sequence_rev_comp)
+        pam_positions = [match.start() for match in matches]
+
+        for position in pam_positions:
+            if (position - toward_5_prime >= 0):
+                guide = sequence_rev_comp[position-toward_5_prime:position-1]
+                guides_list.append((guide, 'R', numpy.random.rand(1)[0] * 10))
+
+        return guides_list
