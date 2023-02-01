9802f5c9d8817914c90d685a85039bb815bea4d1
AmirUCR <amohs002@ucr.edu>
2023-01-31 20:05:34 -0500

    Add docstring to base class.

--- src/scorers/scorer_base.py
+++ src/scorers/scorer_base.py
@@ -6,5 +6,21 @@ class Scorer:
         pass
     
 
-    def score_sequence(self, guide_container: GuideContainer) -> list[tuple[str, str, float, int]]:
+    def score_sequence(
+        self,
+        guide_container: GuideContainer,
+        ) -> tuple[list[str], list[str], list[int], list[float]]:
+        '''
+        ## Args:
+            * guide_container: Either a Gene or a Chromosome type guide container.
+        
+        ## Returns:
+            A tuple of four lists:
+            * The first list[str] is a list of the guides found in `sequence`.
+            * The seconds list[str] is a list of 'F's and 'R's indicating on
+              which strand, forward or reverse, each respective guide resides.
+            * The third list[int] shows the locations of each guides in `sequence`.
+            * The fourth list[float] indicates the efficiency scores of each guide.
+        '''
+        
         raise NotImplementedError
