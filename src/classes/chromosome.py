6019dbae8001310d6e141501777f756e710ff6a3
AmirUCR <amohs002@ucr.edu>
2023-02-05 18:56:56 -0800

    No longer saves list of guides internally.

--- src/classes/chromosome.py
+++ src/classes/chromosome.py
@@ -4,15 +4,13 @@ from classes.guide_container import GuideContainer
 
 
 class Chromosome(GuideContainer):
-    __slots__ = ['sequence', 'string_id', 'integer_id', 'species_name',
-    'guide_scorer', 'cas9_guide_objects']
+    __slots__ = ['sequence', 'string_id', 'integer_id', 'species_name', 'guide_scorer']
 
     sequence: str
     string_id: str
     integer_id: int
     species_name: str
     guide_scorer: Scorer
-    cas9_guide_objects: list[Guide]
 
     def __init__(
         self,
@@ -29,37 +27,35 @@ class Chromosome(GuideContainer):
         self.species_name = species_name
         self.guide_scorer = guide_scorer
 
-        self.cas9_guide_objects = list()
-        
 
     def get_cas9_guides(self) -> list[Guide]:
-        if len(self.cas9_guide_objects) == 0:
+        self.cas9_guide_objects: list[Guide] = list()
 
-            (guides_list,
-            guides_context_list,
-            strands_list, 
-            locations_list,
-            scores_list) = self.guide_scorer.score_sequence(self)
+        (guides_list,
+        guides_context_list,
+        strands_list, 
+        locations_list,
+        scores_list) = self.guide_scorer.score_sequence(self)
 
-            for i in range(len(guides_list)):
-                self.cas9_guide_objects.append(Guide(
-                    pam='GG',
-                    container=self,
-                    endonuclease='cas9',
-                    score=scores_list[i],
-                    strand=strands_list[i],
-                    sequence=guides_list[i],
-                    genomic_location=locations_list[i],
-                    sequence_with_context=guides_context_list[i]
-                    )
+        for i in range(len(guides_list)):
+            self.cas9_guide_objects.append(Guide(
+                pam='GG',
+                endonuclease='cas9',
+                score=scores_list[i],
+                strand=strands_list[i],
+                sequence=guides_list[i],
+                genomic_location=locations_list[i],
+                sequence_with_context=guides_context_list[i],
+                guide_container_metadata_dict=self.get_attributes_dict(),
                 )
+            )
 
         self.sequence = ''
 
         return self.cas9_guide_objects
 
 
-    def get_attributes_dict(self) -> dict:
+    def get_attributes_dict(self) -> dict[str, str | int]:
         return dict({
             'genome_string_id': self.string_id,
             'genome_integer_id': self.integer_id,
