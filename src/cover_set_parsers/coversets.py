e2e5032bde19181502a8be76bd8aac9276c6e639
AmirUCR <amohs002@ucr.edu>
2023-02-05 16:27:31 -0800

    Implement __slots__. Do not store all Species objs

--- src/cover_set_parsers/coversets.py
+++ src/cover_set_parsers/coversets.py
@@ -8,30 +8,46 @@ from classes.guide_container_factory import GuideContainerFactory
 
 
 class Coversets:
+    __slots__ = ['scorer_name', 'cas_variant', 'guide_source', 'input_cds_directory',
+    'input_genome_directory', 'scorer_settings', 'input_species_csv_file_path',
+    'species_set', 'species_names', 'seq_to_guides_dict', 'coversets']
+
+    scorer_name: str
+    cas_variant: str
+    guide_source: str
+    input_cds_directory: str
+    input_genome_directory: str
+    scorer_settings: dict[str, str]
+    input_species_csv_file_path: str
+    species_set: set[int]
+    species_names: list[str]
+    seq_to_guides_dict: dict[str, list[Guide]]
+    coversets: dict[str, tuple[float, set[int]]]
+
     def __init__(
-        self, 
-        scorer_name: str, 
+        self,
+        scorer_name: str,
         cas_variant: str,
         guide_source: str,
         input_cds_directory: str,
-        input_genome_directory: str, 
+        input_genome_directory: str,
         scorer_settings: dict[str, str],
         input_species_csv_file_path: str,
         ) -> None:
 
-        self.species_set: set[int] = set()
-        self.species_names: list[str] = list()
-        self.int_to_species_dict: dict[int, Species] = dict()
-        self.seq_to_guides_dict: dict[str, list[Guide]] = dict()
+        self.species_set = set()
+        self.species_names = list()
+        # self.int_to_species_dict: dict[int, Species] = dict()
+        self.seq_to_guides_dict = dict()
 
         # To be returned for the solver to use
-        self.cover_sets: dict[str, tuple[float, set[int]]] = dict()
+        self.coversets = dict()
 
         scorer_factory = ScorerFactory()
         scorer = scorer_factory.make_scorer(
-            scorer_name=scorer_name, 
+            scorer_name=scorer_name,
             scorer_settings=scorer_settings,
-            )
+        )
 
         guide_container_factory = GuideContainerFactory()
 
@@ -48,7 +64,7 @@ class Coversets:
             genome_path = os.path.join(input_genome_directory, row.genome_file_name)
 
             # {0: Species(name='kmarxianus'), 1: Species(name='scerevisiae'), ...}
-            self.int_to_species_dict[idx] = Species(
+            species_object = Species(
                 id=idx,
                 cds_path=cds_path,
                 guide_scorer=scorer,
@@ -57,14 +73,6 @@ class Coversets:
                 guide_source=guide_source,
                 guide_container_factory=guide_container_factory,
             )
-
-        print('Creating coversets for each species.')
-
-        count = 0
-        # For each species, get the cas9 guide objects, and create coversets from their sequences
-        for species_id, species_object in self.int_to_species_dict.items():
-            if count % 50 == 0: print('Done with', count, 'species...')
-            count += 1
 
             guide_objects_list: list[Guide] = list()
 
@@ -85,16 +93,20 @@ class Coversets:
                 # assign their average score to a unique representative guide 
                 # that is input to the solver.
                 list_of_guides_with_this_sequence = self.seq_to_guides_dict[sequence]
-                average_score = self.get_average_score_for_guide_objects(list_of_guides_with_this_sequence)
+                average_score = self.get_average_score_for_guide_objects(
+                    list_of_guides_with_this_sequence,
+                )
 
-                # TODO: A little hacky whacky -- cover_sets should be an object?
-                if sequence in self.cover_sets:
-                    self.cover_sets[sequence][1].add(species_id)
+                # TODO: A little hacky whacky -- coversets should be an object?
+                if sequence in self.coversets:
+                    self.coversets[sequence][1].add(idx)
                 else:
-                    self.cover_sets[sequence] = tuple((average_score, set({species_id})))  # type: ignore
-        
+                    self.coversets[sequence] = tuple((average_score, set({idx})))  # type: ignore
+            
+            print('Done with', idx + 1, 'species...')
+
         print('Created coversets for all species.')
-                
+
 
     def get_average_score_for_guide_objects(
         self,
