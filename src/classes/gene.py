47bd71048023d47b14354993661c8fd07a01699d
AmirUCR <amohs002@ucr.edu>
2023-02-05 16:24:11 -0800

    Implement __slots__

--- src/classes/gene.py
+++ src/classes/gene.py
@@ -1,15 +1,26 @@
-from __future__ import annotations
-import typing
-
-if typing.TYPE_CHECKING:
-    from classes.species import Species
-
 from classes.guide import Guide
 from scorers.scorer_base import Scorer
 from classes.guide_container import GuideContainer
 
 
 class Gene(GuideContainer):
+    __slots__ = ['sequence', 'gene_name', 'locus_tag', 'string_id', 'integer_id',
+    'protein_id', 'species_name', 'ref_species', 'guide_scorer', 'orthologous_to_gene',
+    'orthologous_to_prot', 'cas9_guide_objects']
+    
+    sequence: str
+    gene_name: str
+    locus_tag: str
+    string_id: str
+    integer_id: int
+    protein_id: str
+    species_name: str
+    ref_species: str
+    guide_scorer: Scorer
+    orthologous_to_gene: str
+    orthologous_to_prot: str
+    cas9_guide_objects: list[Guide]
+
     def __init__(
         self,
         sequence: str,
@@ -18,7 +29,7 @@ class Gene(GuideContainer):
         string_id: str,
         integer_id: int,
         protein_id: str,
-        species: Species,
+        species_name: str,
         ref_species: str,
         guide_scorer: Scorer,
         orthologous_to_gene: str,
@@ -31,27 +42,23 @@ class Gene(GuideContainer):
         self.string_id = string_id
         self.integer_id = integer_id
         self.protein_id = protein_id
-        self.species = species  # Which species does this gene belong to?
         self.ref_species = ref_species  # Which reference species are we using? 
                                         # This is found in utils/find_orthogroup_config
                                         # under the species attribute.
+        self.species_name = species_name  # Which species does this gene belong to?
         self.guide_scorer = guide_scorer  # Any assigned scorer in scorers/
         self.orthologous_to_gene = orthologous_to_gene  # Which input gene is this gene orthologous to?
         self.orthologous_to_prot = orthologous_to_prot  # Which input protein_id is this gene orthologous to?
                                                         # This is the protein_id of the orthologous_to_gene
 
-        self.cas9_guide_objects: list[Guide] = list()
-
-
-    @property
-    def species_name(self) -> str:
-        return self.species.name
+        self.cas9_guide_objects = list()
 
 
     def get_cas9_guides(self) -> list[Guide]:
         if len(self.cas9_guide_objects) == 0:
             
             (guides_list,
+            guides_context_list,
             strands_list, 
             locations_list,
             scores_list) = self.guide_scorer.score_sequence(self)
@@ -65,6 +72,7 @@ class Gene(GuideContainer):
                     strand=strands_list[i],
                     sequence=guides_list[i],
                     genomic_location=locations_list[i],
+                    sequence_with_context=guides_context_list[i]
                     )
                 )
             
