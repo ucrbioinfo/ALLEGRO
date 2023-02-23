6b9f6518b7b3ba4c40358d8fffddf4769bf45589
AmirUCR <amohs002@ucr.edu>
2023-02-22 19:09:25 -0800

    Formatting and fix incorrect type hints

--- src/scorers/dummy_scorer.py
+++ src/scorers/dummy_scorer.py
@@ -8,30 +8,28 @@ class DummyScorer(Scorer):
     'context_toward_five_prime', 'context_toward_three_prime']
 
     pam: str
-    protospacer_length: str
-    context_toward_five_prime: str
-    context_toward_three_prime: str
+    protospacer_length: int
+    context_toward_five_prime: int
+    context_toward_three_prime: int
 
     def __init__(self, settings: dict[str, str]) -> None:
-        super().__init__(settings=settings)
-
         self.pam = settings['pam']
-        self.protospacer_length = settings['protospacer_length']
-        self.context_toward_five_prime = settings['context_toward_five_prime']
-        self.context_toward_three_prime = settings['context_toward_three_prime']
+        self.protospacer_length = int(settings['protospacer_length'])
+        self.context_toward_five_prime = int(settings['context_toward_five_prime'])
+        self.context_toward_three_prime = int(settings['context_toward_three_prime'])
         
 
     def score_sequence(
         self,
         guide_container: GuideContainer
-        ) -> tuple[list[str], list[str], list[str], list[str], list[int], list[float]]:
+        ) -> tuple[list[str], list[str], list[str], list[int], list[float]]:
         '''
-        Assigns all guides in param `guide_container` a score of 1.0
+        Assigns all guides in param sequence a score of 1.0
         '''
 
-        (guides_list, 
+        (guides_list,
         guides_context_list,
-        strands_list, 
+        strands_list,
         locations_list) = find_guides_and_indicate_strand(
             pam=self.pam,
             sequence=guide_container.sequence,
