fa6ce2678751777a3a63dfe8c651acfd0eb90a58
AmirUCR <amohs002@ucr.edu>
2023-02-22 19:10:07 -0800

    Make class a proper abstract class

--- src/scorers/scorer_base.py
+++ src/scorers/scorer_base.py
@@ -1,11 +1,14 @@
-from classes.guide_container import GuideContainer
+from __future__ import annotations
+import typing
+
+if typing.TYPE_CHECKING:
+    from classes.guide_container import GuideContainer
+
+from abc import ABC, abstractmethod
 
 
-class Scorer:
-    def __init__(self, settings: dict[str, str]) -> None:
-        pass
-    
-
+class Scorer(ABC):
+    @abstractmethod
     def score_sequence(
         self,
         guide_container: GuideContainer,
@@ -18,10 +21,8 @@ class Scorer:
             A tuple of four lists:
             * The first list[str] is a list of the guides found in `sequence`.
             * The second list[str] is a list of guides in `sequence` with context around them.
-            * The third list[str] is a list of 'F's and 'R's indicating on
-              which strand, forward or reverse, each respective guide resides.
+            * The third list[str] is a list of '0.0's and '1.0's indicating on
+              which strand, forward (0.0) or reverse complement (1.0), each respective guide resides.
             * The fourth list[int] shows the locations of each guides in `sequence`.
             * The fifth list[float] indicates the efficiency scores of each guide.
         '''
-        
-        raise NotImplementedError
