de942a3f10e7d65d21bad6f7f3737b6d69ed54a7
AmirUCR <amohs002@ucr.edu>
2023-03-15 18:30:32 -0700

    Formatting. Now instantiates guide_finder.

--- src/scorers/dummy_scorer.py
+++ src/scorers/dummy_scorer.py
@@ -1,17 +1,24 @@
 from classes.guide_container import GuideContainer
 from scorers.scorer_base import Scorer
-from utils.find_guides_in_seq import find_guides_and_indicate_strand
+from utils.guide_finder import GuideFinder
 
 
 class DummyScorer(Scorer):
-    __slots__ = ['pam', 'protospacer_length', 'include_repetitive',
-    'context_toward_five_prime', 'context_toward_three_prime']
+    __slots__ = [
+        'pam',
+        'protospacer_length',
+        'include_repetitive',
+        'context_toward_five_prime',
+        'context_toward_three_prime',
+        'guide_finder'
+        ]
 
     pam: str
     protospacer_length: int
     include_repetitive: bool
     context_toward_five_prime: int
     context_toward_three_prime: int
+    guide_finder: GuideFinder
 
     def __init__(self, settings: dict) -> None:
         self.pam = settings['pam']
@@ -19,20 +26,21 @@ class DummyScorer(Scorer):
         self.protospacer_length = settings['protospacer_length']
         self.context_toward_five_prime = settings['context_toward_five_prime']
         self.context_toward_three_prime = settings['context_toward_three_prime']
+        self.guide_finder = GuideFinder()
         
 
     def score_sequence(
         self,
         guide_container: GuideContainer
-        ) -> tuple[list[str], list[str], list[str], list[int], list[float]]:
+        ) -> tuple[list[str], list[str], list[str], list[int], list[int]]:
         '''
-        Assigns all guides in param sequence a score of 1.0
+        Assigns all guides in param sequence a score of 1
         '''
 
         (guides_list,
         guides_context_list,
         strands_list,
-        locations_list) = find_guides_and_indicate_strand(
+        locations_list) = self.guide_finder.find_guides_and_indicate_strand(
             pam=self.pam,
             sequence=guide_container.sequence,
             protospacer_length=self.protospacer_length,
@@ -41,6 +49,6 @@ class DummyScorer(Scorer):
             include_repetitive=self.include_repetitive
         )
         
-        one_scores = [1.0] * len(guides_list)
+        scores = [1] * len(guides_list)
         
-        return guides_list, guides_context_list, strands_list, locations_list, one_scores
+        return guides_list, guides_context_list, strands_list, locations_list, scores
