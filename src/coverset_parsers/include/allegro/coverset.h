67144b3d080aa1029db331add7030452c9e7f684
AmirUCR <amohs002@ucr.edu>
2023-03-15 18:30:09 -0700

    Ongoing improvements.

--- src/coverset_parsers/include/allegro/coverset.h
+++ src/coverset_parsers/include/allegro/coverset.h
@@ -9,31 +9,37 @@
 #include <boost/dynamic_bitset.hpp>
 #include "ortools/linear_solver/linear_solver.h"
 
+namespace coversets
+{
+    class CoversetCPP
+    {
+    private:
+        std::size_t num_trials;
+        std::size_t num_species;
+        std::size_t guide_length;
+        std::size_t bits_required_to_store_seq;
+        boost::dynamic_bitset<> all_species_bitset;
 
-namespace coversets {
-    class CoversetCPP {
-        private:
-            std::size_t bits_required_to_store_seq;
-            boost::dynamic_bitset<> all_species_bitset;
-            std::string decode_bitset(std::string encoded_str);
-            std::string decode_bitset(boost::dynamic_bitset<> encoded);
+        // guide --> bitvector of species it hits
+        std::unordered_map<boost::dynamic_bitset<>, std::pair<unsigned char, boost::dynamic_bitset<>>> coversets;
 
-            void randomized_rounding(std::vector<operations_research::MPVariable*> feasible_solutions);
-        public:
-            std::size_t num_trials;
-            std::size_t num_species;
-            std::size_t guide_length;
-            // std::unordered_map<boost::dynamic_bitset<>, std::pair<unsigned char, boost::dynamic_bitset<>>> coversets;
+        std::string get_str();
+        std::string decode_bitset(const std::string &encoded_str);
+        std::string decode_bitset(boost::dynamic_bitset<> &encoded);
+
+        std::unordered_set<std::string> randomized_rounding(
+            std::vector<operations_research::MPVariable *> feasible_solutions);
+
+    public:
+        CoversetCPP(
+            std::size_t num_species,
+            std::size_t guide_length, 
+            std::size_t num_trials);
             
-            // guide --> bitvector of species it hits 
-            std::unordered_map<boost::dynamic_bitset<>, boost::dynamic_bitset<>> coversets;
+        ~CoversetCPP();
 
-            CoversetCPP(std::size_t num_species, std::size_t guide_length, std::size_t num_trials);
-            ~CoversetCPP();
-
-            void ortools_solver();
-            std::string get_str();
-            void encode_and_save_dna(std::string& seq, unsigned char score, unsigned short species_id);
+        std::unordered_set<std::string> ortools_solver(); // to be removed
+        void encode_and_save_dna(std::string &seq, unsigned char score, unsigned short species_id);
     };
 }
 
