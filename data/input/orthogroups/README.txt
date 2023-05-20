881685bad63c28bf1a619204a6cbb1166588ff2d
AmirUCR <amohs002@ucr.edu>
2023-05-19 18:47:56 -0700

    Minor change

---
+++ data/input/orthogroups/README.txt
@@ -0,0 +1,5 @@
+Orthogroups_old_from_orthofinder.tsv was generated long ago by orthofinder.
+
+Orthofinder.tsv is generated from diamond for 
+
+'LYS2', 'LYS5', 'MET17', 'TRP1', 'URA3', 'FCY1', 'GAP1', 'CAN1'
---
+++ src/cython_libs/build/CMakeFiles/coverset2.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coverset2.dir/coverset2.cpp.o"
+  "CMakeFiles/coverset2.dir/coverset2.cpp.o.d"
+  "CMakeFiles/coverset2.dir/coverset2.cxx.o"
+  "CMakeFiles/coverset2.dir/coverset2.cxx.o.d"
+  "CMakeFiles/coverset2.dir/decode_bitset.cpp.o"
+  "CMakeFiles/coverset2.dir/decode_bitset.cpp.o.d"
+  "CMakeFiles/coverset2.dir/ilp_approximators.cpp.o"
+  "CMakeFiles/coverset2.dir/ilp_approximators.cpp.o.d"
+  "CMakeFiles/coverset2.dir/logger.cpp.o"
+  "CMakeFiles/coverset2.dir/logger.cpp.o.d"
+  "coverset2.cxx"
+  "coverset2.pdb"
+  "coverset2.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coverset2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
