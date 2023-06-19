55ecbbcaf5adee797196c0759bf6364fff957361
AmirUCR <amohs002@ucr.edu>
2023-06-19 16:03:36 -0700

    Data type changes

--- src/cython_libs/include/allegro/kirschtorte.h
+++ src/cython_libs/include/allegro/kirschtorte.h
@@ -14,6 +14,7 @@
 #include "allegro/logger.h"
 #include "allegro/decorators.h"
 #include "allegro/definitions.h"
+#include "allegro/guide_struct.h"
 #include "allegro/decode_bitset.h"
 #include "allegro/ilp_approximators.h"
 
@@ -23,6 +24,25 @@ namespace Kirschtorte
 {
     class Kirschtorte
     {
+    public:
+        Kirschtorte(
+            std::size_t num_containers,
+            std::size_t guide_length,
+            std::size_t num_trials,
+            std::string output_directory);
+
+        ~Kirschtorte();
+
+        std::vector<GuideStruct> setup_and_solve(
+            std::size_t monophonic_threshold,
+            std::size_t cut_multiplicity,
+            std::size_t beta);
+
+        int encode_and_save_dna(
+            std::string seq,
+            double score,
+            std::size_t container_id);
+
     private:
         std::size_t num_trials;
         std::size_t num_containers;
@@ -34,26 +54,7 @@ namespace Kirschtorte
 
         // guide bitvector --> (score, bitvector of species it hits)
         // The width of the guide bitvector is 2 * guide length AKA this->(std::size_t) bits_required_to_store_seq
-        std::map<boost::dynamic_bitset<>, std::pair<char, boost::dynamic_bitset<>>> coversets;
-
-    public:
-        Kirschtorte(
-            std::size_t num_containers,
-            std::size_t guide_length,
-            std::size_t num_trials,
-            std::string output_directory);
-
-        ~Kirschtorte();
-
-        std::vector<std::pair<std::string, std::string>> setup_and_solve(
-            std::size_t monophonic_threshold,
-            std::size_t cut_multiplicity,
-            std::size_t beta);
-
-        int encode_and_save_dna(
-            std::string seq,
-            std::size_t score,
-            std::size_t container_id);
+        std::map<boost::dynamic_bitset<>, std::pair<double, boost::dynamic_bitset<>>> coversets;
     };
 }
 
