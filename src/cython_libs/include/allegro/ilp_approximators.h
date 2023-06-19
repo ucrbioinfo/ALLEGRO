968c08286c5fdef32d4264624cd594803ecf44bc
AmirUCR <amohs002@ucr.edu>
2023-06-19 16:03:29 -0700

    Data type changes

--- src/cython_libs/include/allegro/ilp_approximators.h
+++ src/cython_libs/include/allegro/ilp_approximators.h
@@ -10,13 +10,14 @@
 #include <boost/dynamic_bitset.hpp>
 
 #include "allegro/definitions.h"
+#include "allegro/guide_struct.h"
 #include "allegro/decode_bitset.h"
 #include "ortools/linear_solver/linear_solver.h"
 
-std::vector<std::pair<std::string, std::string>> randomized_rounding(
+std::vector<GuideStruct> randomized_rounding(
     std::vector<operations_research::MPVariable *> &feasible_solutions,
     boost::dynamic_bitset<> all_containers_bitset,
-    std::map<boost::dynamic_bitset<>, std::pair<char, boost::dynamic_bitset<>>> &coversets,
+    std::map<boost::dynamic_bitset<>, std::pair<double, boost::dynamic_bitset<>>> &coversets,
     std::size_t multiplicity,
     std::size_t num_trials,
     std::ostringstream &log_buffer);
