d96147169fa2ed6101ef4603dea50b7f71fd36d9
AmirUCR <amohs002@ucr.edu>
2023-06-19 15:55:42 -0700

    Removed old test files

--- bitset_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/dynamic_bitset.hpp>
-#include <iostream>
-
-int main() {
-    boost::dynamic_bitset<> bitset(5, 0b11111);
-    std::cout << "Original bitset: " << bitset << std::endl;
-
-    return 0;
-}
