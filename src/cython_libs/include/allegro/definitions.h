5794d4e36a82712e20026ff4eeb26c79de4d2e3d
AmirUCR <amohs002@ucr.edu>
2023-06-19 16:02:52 -0700

    Remove unused code

--- src/cython_libs/include/allegro/definitions.h
+++ src/cython_libs/include/allegro/definitions.h
@@ -6,14 +6,4 @@
 #define sG_SHIFT "10"
 #define sT_SHIFT "11"
 
-// template <typename T0, typename T1, typename T2>
-// struct TUPLE_3 {
-//     T0 value0;
-//     T1 value1;
-//     T2 value2;
-
-//     TUPLE_3(T0 value0, T1 value1, T2 value2):
-//         value0(value0), value1(value1), value2(value2) {}
-// };
-
 #endif
