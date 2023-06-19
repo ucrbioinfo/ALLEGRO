03dd0b1be33f642fec5c19cf214f68fcfcf023bb
AmirUCR <amohs002@ucr.edu>
2023-06-19 16:02:44 -0700

    Change score data type to double

--- src/cython_libs/include/allegro/decorators.h
+++ src/cython_libs/include/allegro/decorators.h
@@ -11,6 +11,6 @@ void decorate_with_monophonic(
     std::size_t cut_multiplicity,
     std::size_t monophonic_threshold,
     std::ostringstream &log_buffer,
-    std::map<boost::dynamic_bitset<>, std::pair<char, boost::dynamic_bitset<>>> &coversets);
+    std::map<boost::dynamic_bitset<>, std::pair<double, boost::dynamic_bitset<>>> &coversets);
 
 #endif
