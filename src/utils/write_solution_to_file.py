d87d0f54bd9f6d4992fb3ff0c33807ce51365673
AmirUCR <amohs002@ucr.edu>
2023-06-19 16:05:14 -0700

    Implement scoring and move things around

--- src/utils/write_solution_to_file.py
+++ src/utils/write_solution_to_file.py
@@ -1,82 +1,16 @@
 # Utility functions imported by main.py.
 # These do not need to be run manually.
-
 import os
-import numpy
 import pandas
-import matplotlib.pyplot
+# import numpy
+# import matplotlib.pyplot
+# matplotlib.pyplot.rcParams['figure.dpi'] = 300
 
 from utils.guide_finder import GuideFinder
 
-matplotlib.pyplot.rcParams['figure.dpi'] = 300
-
-def graph_size_dist(
-    beta: int,
-    exp_name: str, 
-    output_dir: str,
-    size_of_solutions_for_n_trials: list[int],
-    ) -> None:
-
-    print('Drawing size distribution graph...')
-    output_path = os.path.join(output_dir, exp_name + '_b' + str(beta) + '_size_hist.png')
-
-    y = numpy.asarray(size_of_solutions_for_n_trials)
-    average_size_over_all_trials = numpy.mean(y)
-
-    n = len(size_of_solutions_for_n_trials)
-    x = numpy.arange(n)
-
-    title_1 = 'Set size distribution for {n} trials'.format(n=len(size_of_solutions_for_n_trials))
-    title_2 = 'Average size over all trials: {n:.2f}'.format(n=average_size_over_all_trials)
-    title_3 = 'Beta: {b}'.format(b=beta)
-
-    mininum_size = min(size_of_solutions_for_n_trials)
-    red_label = 'Smallest size: {n}'.format(n=mininum_size)
-    blue_mask = y != mininum_size
-    red_mask = y == mininum_size
-
-    matplotlib.pyplot.figure(figsize=(20, 3))
-
-    matplotlib.pyplot.bar(x=x[blue_mask], height=y[blue_mask])
-    matplotlib.pyplot.bar(x=x[red_mask], height=y[red_mask], color='red', label=red_label)
-
-    matplotlib.pyplot.legend()
-    matplotlib.pyplot.title(title_1 + '\n' + title_2 + '\n' + title_3)
-    matplotlib.pyplot.xlabel('Trial')
-    matplotlib.pyplot.ylabel('Set size')
-    matplotlib.pyplot.margins()
-
-    matplotlib.pyplot.tight_layout()
-    matplotlib.pyplot.savefig(output_path)
-
-
-def graph_score_dist(
-    beta: int,
-    exp_name: str, 
-    output_dir: str,
-    average_scores_for_n_trials: list[float],
-    ) -> None:
-
-    print('Drawing score distribution graph...')
-    output_path = os.path.join(output_dir, exp_name + '_b' + str(beta) + '_avg_score_hist.png')
-
-    title = 'Solution guides\' average score distribution over {n} trials'.format(n=len(average_scores_for_n_trials))
-
-    matplotlib.pyplot.figure(figsize=(10, 3))
-
-    matplotlib.pyplot.hist(average_scores_for_n_trials, edgecolor='black', linewidth=1.2)
-    
-    matplotlib.pyplot.title(title)
-    matplotlib.pyplot.xlabel('Average score')
-    matplotlib.pyplot.ylabel('Count')
-
-    matplotlib.pyplot.tight_layout()
-    matplotlib.pyplot.savefig(output_path)
-
-
 def write_solution_to_file(
     species_names: list[str],
-    solution: list[tuple[str, str]],
+    solution: list[tuple[str, float, str]],
     experiment_name: str,
     input_csv_path: str,
     input_directory: str,
@@ -88,31 +22,22 @@ def write_solution_to_file(
     output_txt_path = os.path.join(output_directory, experiment_name + '.txt')
     output_csv_path = os.path.join(output_directory, experiment_name + '.csv')
 
-    print('Writing to file:', output_txt_path)
+    print(f'Writing to file: {output_txt_path}')
     with open(output_txt_path, 'w') as f:
 
-        for tuple_elem in solution:
-            f.write('Guide {g} targets {n} genes/chromosomes.\n'.format(
-                g=tuple_elem[0],
-                n=len(tuple_elem[1])
-            ))
+        for tup in solution:
+            seq = tup[0]
+            score = tup[1]
+            hit_species = tup[2]
 
-        f.write('We can cut the following {n} species: {species}.\n'.format(
-            n=len(species_names),
-            species=str(species_names),
-            )
-        )
-
-        f.write('Using the following {n} guides: {guides}.\n'.format(
-            n=str(len(solution)),
-            guides=str(solution),
-            )
-        )
+            f.write(f'Guide {seq} targets {len(hit_species)} genes/chromosomes.\n')
+        f.write(f'We can cut the following {len(species_names)} species: {str(species_names)}.\n')
+        f.write(f'Using the following {str(len(solution))} guides: {str(solution)}.\n')
 
     paths: list[str] = list()
     misc_list: list[str] = list()
     species_list: list[str] = list()
-    scores: list[int] = list()
+    scores: list[float] = list()
     strands: list[str] = list()
     sequences: list[str] = list()
     end_positions: list[int] = list()
@@ -124,8 +49,8 @@ def write_solution_to_file(
 
     for tup in solution:
         seq = tup[0]
-        # score = tup[1]
-        hit_species = tup[1]
+        score = tup[1]
+        hit_species = tup[2]
 
         for species_gene_tupe in hit_species:
             species_gene_tupe = species_gene_tupe.split(', ')
@@ -148,7 +73,7 @@ def write_solution_to_file(
 
                 sequences.append(seq)
                 paths.append(df_file_path)
-                scores.append(1)
+                scores.append(score)
                 strands.append(strand)
                 start_positions.append(start_pos)
                 end_positions.append(end_pos)
@@ -168,50 +93,114 @@ def write_solution_to_file(
         misc_list,
         paths,
         )),
-    columns=['sequence', 'targets', 'score', 'chromosome_or_gene',
+    columns=['sequence', 'target', 'score', 'chromosome_or_gene',
     'strand', 'start_position', 'end_position', 'misc', 'path']).to_csv(output_csv_path, index=False)
     
-    print('Done. Check {path} for the output.'.format(path=output_csv_path))
+    print(f'Done. Check {output_csv_path} for the output.')
     return output_csv_path
 
 
-def output_csv(
-    beta: int,
-    experiment_name: str,
-    output_directory: str, 
-    solver,
-    ) -> str:
+# def graph_size_dist(
+#     beta: int,
+#     exp_name: str, 
+#     output_dir: str,
+#     size_of_solutions_for_n_trials: list[int],
+#     ) -> None:
 
-    output_path = os.path.join(output_directory, experiment_name + '_metrics.csv'.format(b=beta))
+#     print('Drawing size distribution graph...')
+#     output_path = os.path.join(output_dir, exp_name + '_b' + str(beta) + '_size_hist.png')
 
-    print('Writing to file:', output_path)
+#     y = numpy.asarray(size_of_solutions_for_n_trials)
+#     average_size_over_all_trials = numpy.mean(y)
 
-    avg_num_while_iters_for_n_trials = 0
-    if len(solver.num_while_iters_for_each_trial) > 0:
-        avg_num_while_iters_for_n_trials = numpy.mean(solver.num_while_iters_for_each_trial).round(2)
+#     n = len(size_of_solutions_for_n_trials)
+#     x = numpy.arange(n)
 
-    df = pandas.DataFrame({
-        'budget': [beta],
-        'avg_score_over_all_trials': [solver.average_score_for_all_trials],
-        'avg_score_over_each_trial': [solver.average_score_for_each_trial],
-        'avg_num_while_iters_for_n_trials': [avg_num_while_iters_for_n_trials],
-        'num_while_iters_for_each_trial': [solver.num_while_iters_for_each_trial],
-        'fractional_glop_vals': [solver.fractional_glop_vals],
-        'solver_time': [solver.solver_time],
-        'exhaustive_time': [solver.exhaustive_time],
-        'random_rounding_time': [solver.randomized_rounding_time],
-        'num_non_zero_feasible': [solver.num_non_zero_feasible],
-        'num_non_zero_feasible_under_one': [solver.num_feasible_guides_with_prob_lt_one],
-        'num_exhausted_combos': [solver.num_exhausted_combos],
-        'solved_with_exhaustive': ['Yes' if solver.solved_with_exhaustive else 'No'],
-        'size_of_solutions_for_n_trials': [solver.set_size_for_each_trial],
-        'num_species_constraints': [len(solver.species)],
-        'num_guide_variables': [solver.k],
-        'num_rounding_trials': [solver.num_trials],
-    })
+#     title_1 = 'Set size distribution for {n} trials'.format(n=len(size_of_solutions_for_n_trials))
+#     title_2 = 'Average size over all trials: {n:.2f}'.format(n=average_size_over_all_trials)
+#     title_3 = 'Beta: {b}'.format(b=beta)
 
-    write_header_if_file_doesnt_exist = not os.path.exists(output_path)  # otherwise append w/ mode='a'
+#     mininum_size = min(size_of_solutions_for_n_trials)
+#     red_label = 'Smallest size: {n}'.format(n=mininum_size)
+#     blue_mask = y != mininum_size
+#     red_mask = y == mininum_size
 
-    df.to_csv(output_path, mode='a', header=write_header_if_file_doesnt_exist, index=False)
-    print('Done. Check {path} for the output.'.format(path=output_path))
-    return output_path
+#     matplotlib.pyplot.figure(figsize=(20, 3))
+
+#     matplotlib.pyplot.bar(x=x[blue_mask], height=y[blue_mask])
+#     matplotlib.pyplot.bar(x=x[red_mask], height=y[red_mask], color='red', label=red_label)
+
+#     matplotlib.pyplot.legend()
+#     matplotlib.pyplot.title(title_1 + '\n' + title_2 + '\n' + title_3)
+#     matplotlib.pyplot.xlabel('Trial')
+#     matplotlib.pyplot.ylabel('Set size')
+#     matplotlib.pyplot.margins()
+
+#     matplotlib.pyplot.tight_layout()
+#     matplotlib.pyplot.savefig(output_path)
+
+
+# def graph_score_dist(
+#     beta: int,
+#     exp_name: str, 
+#     output_dir: str,
+#     average_scores_for_n_trials: list[float],
+#     ) -> None:
+
+#     print('Drawing score distribution graph...')
+#     output_path = os.path.join(output_dir, exp_name + '_b' + str(beta) + '_avg_score_hist.png')
+
+#     title = 'Solution guides\' average score distribution over {n} trials'.format(n=len(average_scores_for_n_trials))
+
+#     matplotlib.pyplot.figure(figsize=(10, 3))
+
+#     matplotlib.pyplot.hist(average_scores_for_n_trials, edgecolor='black', linewidth=1.2)
+    
+#     matplotlib.pyplot.title(title)
+#     matplotlib.pyplot.xlabel('Average score')
+#     matplotlib.pyplot.ylabel('Count')
+
+#     matplotlib.pyplot.tight_layout()
+#     matplotlib.pyplot.savefig(output_path)
+
+
+# def output_csv(
+#     beta: int,
+#     experiment_name: str,
+#     output_directory: str, 
+#     solver,
+#     ) -> str:
+
+#     output_path = os.path.join(output_directory, experiment_name + '_metrics.csv')
+
+#     print(f'Writing to file: {output_path}')
+
+#     avg_num_while_iters_for_n_trials = 0
+#     if len(solver.num_while_iters_for_each_trial) > 0:
+#         avg_num_while_iters_for_n_trials = numpy.mean(solver.num_while_iters_for_each_trial).round(2)
+
+#     df = pandas.DataFrame({
+#         'budget': [beta],
+#         'avg_score_over_all_trials': [solver.average_score_for_all_trials],
+#         'avg_score_over_each_trial': [solver.average_score_for_each_trial],
+#         'avg_num_while_iters_for_n_trials': [avg_num_while_iters_for_n_trials],
+#         'num_while_iters_for_each_trial': [solver.num_while_iters_for_each_trial],
+#         'fractional_glop_vals': [solver.fractional_glop_vals],
+#         'solver_time': [solver.solver_time],
+#         'exhaustive_time': [solver.exhaustive_time],
+#         'random_rounding_time': [solver.randomized_rounding_time],
+#         'num_non_zero_feasible': [solver.num_non_zero_feasible],
+#         'num_non_zero_feasible_under_one': [solver.num_feasible_guides_with_prob_lt_one],
+#         'num_exhausted_combos': [solver.num_exhausted_combos],
+#         'solved_with_exhaustive': ['Yes' if solver.solved_with_exhaustive else 'No'],
+#         'size_of_solutions_for_n_trials': [solver.set_size_for_each_trial],
+#         'num_species_constraints': [len(solver.species)],
+#         'num_guide_variables': [solver.k],
+#         'num_rounding_trials': [solver.num_trials],
+#     })
+
+#     write_header_if_file_doesnt_exist = not os.path.exists(output_path)  # otherwise append w/ mode='a'
+
+#     df.to_csv(output_path, mode='a', header=write_header_if_file_doesnt_exist, index=False)
+#     print(f'Done. Check {output_path} for the output.')
+#     return output_path
