393a776a76029b0e0a16d8dc1cf39e87684a4d7c
AmirUCR <amohs002@ucr.edu>
2023-12-13 14:32:12 -0800

    New feature: Implement post-reported off-target analysis

--- src/utils/postprocessing.py
+++ src/utils/postprocessing.py
@@ -1,6 +1,8 @@
 # Functions imported by ALLEGRO. No need to run it manually.
 import os
+import re
 import pandas
+from threading import Thread, Semaphore, Lock
 
 from utils.offtarget_finder import OfftargetFinder
 from utils.shell_colors import bcolors
@@ -126,60 +128,113 @@ def report_offtargets(input_species_path: str,
                       input_species_offtarget_dir: str,
                       input_species_offtarget_column: str,
                       experiment_name: str,
-                      seed_region_is_n_from_pam: int,
                       num_mismatches: int,
+                      seed_region_is_n_upstream_of_pam: int,
+                      max_threads: int,
                       pam_length: int = 3):
+
+
+    def extract_complete_digits(s):
+            return [int(num) for num in re.findall(r'\d+', s)]
+
+
+    def determine_ot_in_nonseed(digits, guide_len, seed_region_is_n_upstream_of_pam):
+            return any(digit + 1 + seed_region_is_n_upstream_of_pam > guide_len for digit in digits)
     
+
+    # Inner function to use with multithreading. Tasks inside are I/O bound (Bowtie)
+    def find_targets(target_species: str, seed_region_is_n_upstream_of_pam: int) -> None:
+        # Limit access to n concurrent threads (max_threads)
+        with semaphore:
+            target_species_offtarget_dir = os.path.join(input_species_offtarget_dir, species_df[species_df['species_name'] == target_species][input_species_offtarget_column].values[0])
+
+            # Create a Bowtie index for the target species if it doesn't exist (checks in the function)
+            OTF.run_bowtie_build(target_species, target_species_offtarget_dir, background_source)
+            
+            # Get all hits -- Needs to wait for index creation in the previous line
+            all_targets = OTF.run_bowtie_against_other(f'{experiment_name}_output_lib', target_species, background_source, seqs, num_mismatches)
+            
+            all_targets['target_species'] = target_species
+            all_targets['is_off_target'] = '1'  # initially, all hits are off-targets until proven otherwise
+            all_targets['self_targets'] = '0'
+
+            # These are on-targets -- everything else is off-target
+            on_targets = pandas.merge(output_library[output_library['target'] == target_species],
+                                    all_targets,
+                                    left_on=['sequence', 'misc', 'strand', 'start_position'],
+                                    right_on=['sequence', 'reference_name', 'strand', 'start_position'])
+            
+            cols = ['is_off_target', 'self_targets', 'sequence', 'pam', 'mismatch', 'aligned_seq',
+                    'target_species', 'strand', 'reference_name', 'orthologous_to', 'start_position']
+            
+            on_targets = on_targets[cols]  # Only retain certain columns
+            on_targets['is_off_target'] = '0'
+
+            # Merging all_targets with on_targets on columns 'sequence',
+            # 'reference_name', 'strand', 'start_position' to identify matching rows
+            merged_df = all_targets.merge(on_targets,
+                                        on=['sequence', 'reference_name', 'strand', 'start_position'],
+                                        how='left', suffixes=('', '_new'))
+
+            # Updating columns 'is_off_target' and 'orthologous_to' in all_targets where there's a match
+            all_targets['orthologous_to'] = 'N/A'
+            all_targets['is_off_target'] = merged_df['is_off_target_new'].combine_first(all_targets['is_off_target'])
+            all_targets['orthologous_to'] = merged_df['orthologous_to'].combine_first(all_targets['orthologous_to'])
+            all_targets = all_targets[cols]
+
+            # make a list out of the mismatch locations
+            all_targets['digits'] = all_targets['mismatch'].apply(extract_complete_digits)
+
+            all_targets['ot_in_seed'] = all_targets.apply(lambda row: determine_ot_in_nonseed(row['digits'], len(row['sequence']), seed_region_is_n_upstream_of_pam), axis=1)
+            
+            # Remove rows where off-target occurs in the seed region. Remove useless columns.
+            all_targets = all_targets[~all_targets['ot_in_seed'] == True]
+            all_targets = all_targets.drop(columns=['digits', 'ot_in_seed'])
+
+            OT = all_targets[all_targets['is_off_target'] == '1']
+            for seq in OT['sequence'].unique():
+                seq_off_target_species = set(OT[OT['sequence'] == seq]['target_species'])
+                all_target_species = set(all_targets[all_targets['sequence'] == seq]['target_species'])
+
+                if len(seq_off_target_species.intersection(all_target_species)) != 0:
+                    all_targets.loc[all_targets['sequence'] == seq, 'self_targets'] = '1'
+
+            # Limit shared list write access to one thread at a time
+            with lock:
+                created_dfs.append(all_targets)
+    
+
     background_source = 'genome' if 'genome' in input_species_offtarget_column else 'genes'
     
+    lock = Lock()
+    threads: list[Thread] = list()
+    semaphore = Semaphore(max_threads)
+
     OTF = OfftargetFinder()
+    final_df = pandas.DataFrame()
+    created_dfs: list[pandas.DataFrame] = list()  # For threads to deposit their results
     species_df = pandas.read_csv(input_species_path)
     output_library = pandas.read_csv(solution_path)
 
     # Pull back the start position of guides on negative strand
     # by PAM length -- to comply with bowtie hits and consider the PAM
-    output_library.loc[output_library['strand'] == '-', 'start_position'] -= pam_length 
-    seqs = output_library.sequence.unique().tolist()
-    final_df = pandas.DataFrame()
+    output_library.loc[output_library['strand'] == '-', 'start_position'] -= pam_length
     
+    # Get all the library guides
+    seqs = output_library.sequence.unique().tolist()
+
     # Create reads out of guide sequences for Bowtie alignment
     OTF.write_guides_as_reads(f'{experiment_name}_output_lib', seqs)
 
+    # Align the output library against every input species
     for target_species in output_library['target'].unique():
-        target_species_offtarget_dir = os.path.join(input_species_offtarget_dir, species_df[species_df['species_name'] == target_species][input_species_offtarget_column].values[0])
+        thread = Thread(target=find_targets, args=(target_species, seed_region_is_n_upstream_of_pam))
+        thread.start()
+        threads.append(thread)
 
-        OTF.run_bowtie_build(target_species, target_species_offtarget_dir, background_source)
+    # Wait for all threads
+    for thread in threads:
+        thread.join()
 
-        # Get all hits
-        all_targets = OTF.run_bowtie_against_other(f'{experiment_name}_output_lib', target_species, background_source, seqs, seed_region_is_n_from_pam, num_mismatches)
-        
-        all_targets['target_species'] = target_species
-        all_targets['on_off_target'] = 'Off-Target'  # initially, all hits are offtargets until proven otherwise
-
-        # These are on-targets -- everything else is off-target
-        on_targets = pandas.merge(output_library[output_library['target'] == target_species],
-                                 all_targets,
-                                 left_on=['sequence', 'misc', 'strand', 'start_position'],
-                                 right_on=['sequence', 'reference_name', 'strand', 'start_position'])
-        
-        cols = ['on_off_target', 'sequence', 'guide_w_pam', 'num_mutations', 'mismatch', 'aligned_seq',
-                'target_species', 'strand', 'reference_name', 'orthologous_to', 'start_position']
-        
-        on_targets = on_targets[cols]  # Only retain certain columns
-        on_targets['on_off_target'] = 'On-Target'
-
-        # Merging all_targets with on_targets on columns 'sequence', 'reference_name', 'strand', 'start_position'
-        #  to identify matching rows
-        merged_df = all_targets.merge(on_targets,
-                                    on=['sequence', 'reference_name', 'strand', 'start_position'],
-                                    how='left', suffixes=('', '_new'))
-
-        # Updating column 'on_off_target' in all_targets where there's a match
-        all_targets['orthologous_to'] = 'N/A'
-        all_targets['on_off_target'] = merged_df['on_off_target_new'].combine_first(all_targets['on_off_target'])
-        all_targets['orthologous_to'] = merged_df['orthologous_to'].combine_first(all_targets['orthologous_to'])
-        all_targets = all_targets[cols]
-        
-        final_df = pandas.concat([final_df, all_targets], ignore_index=True)
-
+    final_df = pandas.concat(created_dfs, ignore_index=True)
     final_df.to_csv(f'{output_dir}/targets.csv', index=False)
