f7b5ad3939227f68cc6128c284d64a00a7ae2d83
AmirUCR <amohs002@ucr.edu>
2024-02-08 20:03:27 -0800

    Transitioning to cp_model

--- src/cython_libs/ilp_approximators_old.cpp
+++ src/cython_libs/ilp_approximators_old.cpp
@@ -1,6 +1,5 @@
 #include <thread>
 #include <fstream>
-#include <stdlib.h>
 #include <iostream>
 #include <filesystem>
 
@@ -9,14 +8,7 @@
 #include "allegro/ilp_approximators.h"
 
 #include "absl/time/time.h"
-
-#include "ortools/base/logging.h"
-#include "ortools/sat/cp_model.h"
-#include "ortools/sat/cp_model.pb.h"
-#include "ortools/sat/cp_model_solver.h"
-#include "ortools/sat/model.h"
-#include "ortools/sat/sat_parameters.pb.h"
-#include "ortools/util/sorted_interval_list.h"
+#include "ortools/linear_solver/linear_solver.h"
 
 std::vector<GuideStruct> sat_solver(
     std::vector<operations_research::MPVariable *> &feasible_solutions,
@@ -42,36 +34,41 @@ std::vector<GuideStruct> sat_solver(
     }
 
     // Create a linear solver with the ILP SAT backend.
-    // std::unique_ptr<operations_research::MPSolver> solver(operations_research::MPSolver::CreateSolver("SAT"));
-
-    operations_research::sat::CpModelBuilder cp_model;
-
+    std::unique_ptr<operations_research::MPSolver> solver(operations_research::MPSolver::CreateSolver("SAT"));
+    
     // solver->EnableOutput();
     
-    // int time_limit_ms = early_stopping_patience_s * 1000;  // Time limit in milliseconds
-    // absl::Duration time_limit = absl::Milliseconds(time_limit_ms);  // Convert milliseconds to absl::Duration
-    // solver->SetTimeLimit(time_limit);
+    int time_limit_ms = early_stopping_patience_s * 1000;  // Time limit in milliseconds
+    absl::Duration time_limit = absl::Milliseconds(time_limit_ms);  // Convert milliseconds to absl::Duration
+    solver->SetTimeLimit(time_limit);
 
-    // const double infinity = solver->infinity();  // Used for constraints to denote >= 1 and <= 1
+    const double infinity = solver->infinity();  // Used for constraints to denote >= 1 and <= 1
 
     // May return 0 when not able to detect
-    // const auto processor_count = std::thread::hardware_concurrency();
-    // if (processor_count > 1) {
-    //     absl::Status status = solver->SetNumThreads(processor_count);
+    const auto processor_count = std::thread::hardware_concurrency();
+    if (processor_count > 1) {
+        absl::Status status = solver->SetNumThreads(processor_count);
 
-    //     if (!status.ok()) {
-    //         std::cout << RED << "> Error setting number of threads: " << RESET << status.message() << std::endl;
-    //     }
-    // }
+        if (!status.ok()) {
+            std::cout << RED << "> Error setting number of threads: " << RESET << status.message() << std::endl;
+        }
+    }
 
     // --------------------------------------------------
     // -------------- VARIABLE CREATION -----------------
     // --------------------------------------------------
+    operations_research::MPObjective *const objective = solver->MutableObjective();
+    operations_research::MPConstraint *beta_constraint;
+
+    if (beta > 0)
+    {
+        beta_constraint = solver->MakeRowConstraint(-infinity, beta);
+    }
+
     std::map<boost::dynamic_bitset<>, std::set<boost::dynamic_bitset<>>> hit_containers;
 
     // Maps a bitset (representing a protospace sequence) to an OR-TOOLS variable.
-    std::map<boost::dynamic_bitset<>, operations_research::sat::BoolVar> map_seq_to_vars;
-    std::vector<operations_research::sat::BoolVar> all_vars;
+    std::map<boost::dynamic_bitset<>, operations_research::MPVariable *> map_seq_to_vars;
 
     auto it = all_feasible_coverings.begin();
     while (it != all_feasible_coverings.end())
@@ -105,21 +102,19 @@ std::vector<GuideStruct> sat_solver(
 
         std::string buffer;
         boost::to_string(guide_seq_bitset, buffer);
-        operations_research::sat::BoolVar const var = cp_model.NewBoolVar().WithName(buffer);
+        operations_research::MPVariable *const var = solver->MakeIntVar(0, 1, buffer);
 
         map_seq_to_vars[guide_seq_bitset] = var;
 
-        all_vars.push_back(var);
-
-        // if (beta > 0)
-        // {
-        //     objective->SetCoefficient(var, score);
-        //     beta_constraint->SetCoefficient(var, 1);
-        // }
-        // else
-        // {
-        //     objective->SetCoefficient(var, 1);
-        // }
+        if (beta > 0)
+        {
+            objective->SetCoefficient(var, score);
+            beta_constraint->SetCoefficient(var, 1);
+        }
+        else
+        {
+            objective->SetCoefficient(var, 1);
+        }
 
         it++;
     }
@@ -129,7 +124,7 @@ std::vector<GuideStruct> sat_solver(
     // --------------------------------------------------
     for (auto i : hit_containers)
     {
-        std::vector<operations_research::sat::BoolVar> vars_for_this_container;
+        std::vector<operations_research::MPVariable *> vars_for_this_container;
         std::set<boost::dynamic_bitset<>> seq_bitsets_set = i.second;
 
         for (auto j : seq_bitsets_set)
@@ -137,64 +132,68 @@ std::vector<GuideStruct> sat_solver(
             vars_for_this_container.push_back(map_seq_to_vars[j]);
         }
 
-        cp_model.AddGreaterOrEqual(operations_research::sat::LinearExpr::Sum(vars_for_this_container), 1);
+        // All species/genes must be covered by at least multiplicity guide(s)
+        operations_research::MPConstraint *const constraint = solver->MakeRowConstraint(multiplicity, infinity);
+        for (auto k : vars_for_this_container)
+        {
+            constraint->SetCoefficient(k, 1);
+        }
     }
 
     hit_containers.clear(); // Mark memory as free
-
+    
+    // Set the appropriate objective.
     if (beta > 0)
     {
-        cp_model.AddLessOrEqual(operations_research::sat::LinearExpr::Sum(all_vars), beta);
-        cp_model.Maximize(operations_research::sat::LinearExpr::Sum(all_vars));
+        // LOG(INFO) << "Beta: " << beta << " - Maximizing the the set size considering beta." << std::endl;
+        // this->log_buffer << "Beta: " << beta << " - Maximizing the the set size considering beta." << std::endl;
+
+        objective->SetMaximization();
     }
-    else {
-        cp_model.Minimize(operations_research::sat::LinearExpr::Sum(all_vars));
+    else
+    {
+        // LOG(INFO) << "Beta: " << beta << " - Minimizing the the set size. Disregarding scores and beta." << std::endl;
+        // this->log_buffer << "Beta: " << beta << " - Minimizing the the set size. Disregarding scores and beta." << std::endl;
+
+        objective->SetMinimization();
     }
 
-    // Solving part.
-    operations_research::sat::Model model;
-
-    // Sets a time limit.
+    // Solve the integer linear program
     std::cout << BLUE << "> " << RESET << "Solving the ILP within the given time limit of " << early_stopping_patience_s << " seconds..." << std::endl;
-    operations_research::sat::SatParameters parameters;
-    parameters.set_max_time_in_seconds(early_stopping_patience_s);
-    model.Add(NewSatParameters(parameters));
-
-    // Solve.
-    const operations_research::sat::CpSolverResponse response = SolveCpModel(cp_model.Build(), &model);
+    const operations_research::MPSolver::ResultStatus result_status = solver->Solve();
 
     // Check that the problem has a solution.
-    log_buffer << "Status: " << response.status() << std::endl;
-    if (response.status() == operations_research::sat::OPTIMAL)
+    log_buffer << "Status: " << result_status << std::endl;
+    if (result_status == operations_research::MPSolver::OPTIMAL)
     {
-        // std::cout << BLUE << "> Status: " << response.status() << RESET << std::endl;
+        std::cout << BLUE << "> Status: " << result_status << RESET << std::endl;
         std::cout << BLUE << "> " << RESET << "Found an optimal solution!" << std::endl;
         log_buffer << "The ILP has an optimal solution!" << std::endl;
     }
-    else if (response.status() == operations_research::sat::FEASIBLE)
+    else if (result_status == operations_research::MPSolver::FEASIBLE)
     {
-        // std::cout << BLUE << "> Status: " << response.status() << RESET << std::endl;
-        std::cout << BLUE << "> " << RESET << "Found a feasible solution. Increasing the patience may result in a better solution." << std::endl;
+        std::cout << BLUE << "> Status: " << result_status << RESET << std::endl;
+        std::cout << BLUE << "> " << RESET << "Found a feasible solution. Increasing the patience may result in finding an optimal solution." << std::endl;
         log_buffer << "The ILP has a feasible solution." << std::endl;
     }
     else
     {
-        std::cout << RED << "> Status: " << response.status() << RESET << std::endl;
+        std::cout << RED << "> Status: " << result_status << RESET << std::endl;
         std::cout << RED << "> The ILP problem cannot be solved. Exiting." << RESET << std::endl;
         log_buffer << "The ILP problem cannot be solved." << std::endl;
         return std::vector<GuideStruct>();
     }
 
-    std::vector<operations_research::sat::BoolVar> sat_feasible_solutions;
+    std::vector<operations_research::MPVariable *> sat_feasible_solutions;
 
     for (auto i : map_seq_to_vars)
         {
             boost::dynamic_bitset<> seq_bitset = i.first;
-            operations_research::sat::BoolVar var = i.second;
+            operations_research::MPVariable *var = i.second;
 
-            if (operations_research::sat::SolutionIntegerValue(response, var) > 0.0)
+            if (var->solution_value() > 0.0)
             {
-                log_buffer << decode_bitset(seq_bitset) << " with solution value: " << operations_research::sat::SolutionIntegerValue(response, var) << std::endl;
+                log_buffer << decode_bitset(seq_bitset) << " with solution value: " << var->solution_value() << std::endl;
 
                 sat_feasible_solutions.push_back(var);
             }
@@ -210,7 +209,7 @@ std::vector<GuideStruct> sat_solver(
         log_buffer << "The final set consists of:" << std::endl;
         for (auto var_ptr : sat_feasible_solutions)
         {
-            boost::dynamic_bitset<> bitset(var_ptr.Name());
+            boost::dynamic_bitset<> bitset(var_ptr->name());
 
             double score = all_feasible_coverings[bitset].first;
             boost::dynamic_bitset<> species_hit_by_this_guide = all_feasible_coverings[bitset].second;
@@ -218,7 +217,7 @@ std::vector<GuideStruct> sat_solver(
             std::string buffer;
             boost::to_string(species_hit_by_this_guide, buffer);
 
-            std::string decoded_bitset = decode_bitset(var_ptr.Name());
+            std::string decoded_bitset = decode_bitset(var_ptr->name());
 
             GuideStruct guide;
             guide.sequence = decoded_bitset;
