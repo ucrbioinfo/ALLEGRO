82247cb9bfcf97cb4a7a4388bdeeaae49c5a34c1
AmirUCR <amohs002@ucr.edu>
2024-02-15 17:52:19 -0800

    Implement "easy mode", separate solution_writer functions

--- src/main.py
+++ src/main.py
@@ -2,8 +2,9 @@ import sys
 
 from utils.configurator import Configurator
 import utils.postprocessing as postprocessing
-import utils.write_solution_to_file as write_solution
+import utils.write_solution_to_file as solution_writer
 from cython_libs.kirschtorte import KirschtorteCython as coverset  # type: ignore
+from cython_libs.kirschtorte import EinfacherModusCython as einfacher_modus  # type: ignore
 
 
 def main() -> int:
@@ -22,39 +23,72 @@ def main() -> int:
     # Write the current configuration to a log file in the output folder.
     configurator.log_args()
 
-    coversets_obj = coverset(
-        beta=args.beta,
-        track=args.track,
-        early_stopping_patience=args.early_stopping_patience,
-        scorer_name=args.scorer,
-        cas_variant=args.cas,
-        guide_length=20,
-        scorer_settings=scorer_settings,
-        input_directory=args.input_directory,
-        output_directory=args.output_directory,
-        input_species_csv_file_path=args.input_species_path,
-        input_species_path_column=args.input_species_path_column,
-        cut_multiplicity=args.multiplicity,
-        monophonic_threshold=args.mp_threshold,
-        enable_solver_diagnostics=args.enable_solver_diagnostics
-    )
+    if configurator.using_easy_mode:
+        coversets_obj = einfacher_modus(
+            beta=args.beta,
+            track=args.track,
+            early_stopping_patience=args.early_stopping_patience,
+            cas_variant=args.cas,
+            guide_length=20,
+            output_directory=args.output_directory,
+            cut_multiplicity=args.multiplicity,
+            monophonic_threshold=args.mp_threshold,
+            input_csv_path_with_guides=args.input_csv_path_with_guides,
+            enable_solver_diagnostics=args.enable_solver_diagnostics
+        )
 
-    (output_csv_path,
-     total_time_elapsed) = write_solution.write_solution_to_file_bowtie(
-        pam=args.pam,
+        solution_writer.cross_reference_solution_to_input(
+            solution=coversets_obj.solution,
+            experiment_name=args.experiment_name,
+            input_csv_path=args.input_csv_path_with_guides,
+            output_directory=args.output_directory
+        )
+    else:
+        coversets_obj = coverset(
+            beta=args.beta,
+            track=args.track,
+            early_stopping_patience=args.early_stopping_patience,
+            scorer_name=args.scorer,
+            cas_variant=args.cas,
+            guide_length=20,
+            scorer_settings=scorer_settings,
+            input_directory=args.input_directory,
+            output_directory=args.output_directory,
+            input_species_csv_file_path=args.input_species_path,
+            input_species_path_column=args.input_species_path_column,
+            cut_multiplicity=args.multiplicity,
+            monophonic_threshold=args.mp_threshold,
+            enable_solver_diagnostics=args.enable_solver_diagnostics
+        )
+
+        solution_writer.align_solution_to_input_bowtie(
+            pam=args.pam,
+            solution=coversets_obj.solution,
+            experiment_name=args.experiment_name,
+            input_csv_path=args.input_species_path,
+            input_directory=args.input_directory,
+            output_directory=args.output_directory,
+            paths_csv_column_name=args.input_species_path_column,
+            species_names_csv_column_name='species_name'
+        )
+    
+    solution_writer.output_solution_to_text(
         species_names=coversets_obj.species_names,
         solution=coversets_obj.solution,
         experiment_name=args.experiment_name,
-        input_csv_path=args.input_species_path,
-        input_directory=args.input_directory,
-        paths_csv_column_name=args.input_species_path_column,
-        species_names_csv_column_name='species_name',
         output_directory=args.output_directory
-    )
+        )
+
+    solution_writer.output_solution_to_library(
+        solution=coversets_obj.solution,
+        experiment_name=args.experiment_name,
+        output_directory=args.output_directory
+        )
 
     if args.cluster_guides:
         postprocessing.cluster_solution(
-            solution_path=output_csv_path,
+            output_directory=args.output_directory,
+            experiment_name=args.experiment_name,
             req_match_len=args.seed_region_is_n_upstream_of_pam,
             mm_allowed=args.mismatches_allowed_after_seed_region
         )
@@ -62,18 +96,17 @@ def main() -> int:
     if args.output_offtargets:
         postprocessing.report_offtargets(
             input_species_path=args.input_species_path,
-            solution_path=output_csv_path,
-            output_dir=args.output_directory,
+            output_directory=args.output_directory,
+            experiment_name=args.experiment_name,
             input_species_offtarget_dir=args.input_species_offtarget_dir,
             input_species_offtarget_column=args.input_species_offtarget_column,
-            experiment_name=args.experiment_name,
             num_mismatches=args.report_up_to_n_mismatches,
             seed_region_is_n_upstream_of_pam=args.seed_region_is_n_upstream_of_pam,
             pam_length=3
         )
     
     # configurator.cleanup(output_dir=args.output_directory)
-    configurator.log_time(total_time_elapsed)
+    configurator.log_time()
 
     return 0
 
