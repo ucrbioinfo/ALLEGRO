7c8a7a4ffe7a14bd2954a76fd1f08ed80447a7cf
AmirUCR <amohs002@ucr.edu>
2024-02-29 19:45:31 -0800

    Typo

--- src/scorers/scorer_base.py
+++ src/scorers/scorer_base.py
@@ -21,8 +21,8 @@ class Scorer(ABC):
             A tuple of four lists:
             * The first list[str] is a list of the guides found in `guide_container.sequence`.
             * The second list[str] is a list of guides in `guide_container.sequence` with context around them.
-            * The third list[str] is a list of 'F's and 'RC's indicating on
-              which strand, forward (F) or reverse complement (RC), each respective guide resides.
+            * The third list[str] is a list of '-'s and '+'s indicating on
+              which strand, forward (+) or reverse complement (-), each respective guide resides.
             * The fourth list[int] shows the locations of each guides in `guide_container.sequence`.
             * The fifth list[float] indicates the efficiency scores of each guide.
         '''
